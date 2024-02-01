782b02265fd885c23590b9f0079ea1e96b3eaadf
Nicolas Griere <ngriere@gmail.com>
2024-02-01 23:32:07 +0100

    [MAPS] :alembic: Test maps functionalities

--- src/components/simple-map.tsx
+++ src/components/simple-map.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useMemo } from 'react'
 import { MapContainer, TileLayer, Marker, Popup, Polyline } from 'react-leaflet'
 
 
@@ -15,26 +15,43 @@ interface PositionMarker {
 }
 
 const SimpleMap: React.FC<SimpleMapProps> = ({ positionMarkers }) => {
-  console.log("[...positionMarkers[0].startPos, ...positionMarkers[0].endPos]");
-  console.log(positionMarkers[0])
+
+  const [selectedMarkerIndex, setSelectedMarkerIndex] = useState<number>(0);
+
+  const displayedPositionMarker: PositionMarker = useMemo(() => {
+    return positionMarkers[selectedMarkerIndex];
+  }, [positionMarkers, selectedMarkerIndex]);
+
   return (
-    <MapContainer style={{ height: '600px' }} center={CENTER} zoom={4} scrollWheelZoom={true} placeholder={<p>
-      Carte de notre trajet{' '}
-      <noscript>You need to enable JavaScript to see this map.</noscript>
-    </p>}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {positionMarkers.map(positionMarker => <>
-      <Marker position={positionMarker.endPos}>
-        <Popup>
-          {positionMarker.date}
-        </Popup>
-      </Marker>
-      <Polyline pathOptions={{ color: 'blue' }} positions={[positionMarker.startPos, positionMarker.endPos]} />
-      </>)}
-    </MapContainer>
+    <>
+      <label htmlFor="marker-select">Voir la position:&nbsp;</label>
+      <select name="markers" id="marker-select" value={selectedMarkerIndex} onChange={e => setSelectedMarkerIndex(e.target.value)}>
+        <option value={0}>actuelle</option>
+        <option value={7}>il y a 7 jours</option>
+        <option value={15}>il y a 15 jours</option>
+        <option value={30}>il y a 30 jours</option>
+        <option value={positionMarkers.length - 1}>de départ</option>
+      </select>
+      <br /><br />
+      <MapContainer style={{ height: '600px' }} center={CENTER} zoom={4} scrollWheelZoom={true} placeholder={<p>
+        Carte de notre trajet{' '}
+        <noscript>You need to enable JavaScript to see this map.</noscript>
+      </p>}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={displayedPositionMarker.endPos}>
+          <Popup>
+            {displayedPositionMarker.date}
+          </Popup>
+        </Marker>
+        {positionMarkers.map(positionMarker =>
+
+          <Polyline pathOptions={{ color: 'blue' }} positions={[positionMarker.startPos, positionMarker.endPos]} />
+        )}
+      </MapContainer>
+    </>
   )
 }
 
