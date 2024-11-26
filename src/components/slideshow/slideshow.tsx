5f65c4f5269edb8d45f9a10adb657493bc61fe63
Nicolas Griere <ngriere@gmail.com>
2024-11-27 00:17:45 +0100

    Change display of slideshow

--- src/components/slideshow/slideshow.tsx
+++ src/components/slideshow/slideshow.tsx
@@ -28,20 +28,21 @@ const Slideshow: React.FC<SlideshowProps> = ({ picturesUrl, pictureIds }) => {
 
     return (
         <>
+            <h3>Photos du jour</h3>
             <div className="slideshow-container">
                 {pictureIds.map((pictureId, index) => {
                     return <div className="mySlides fade" style={{ display: index === slideIndex ? 'block' : 'none' }}>
                         <img src={`${picturesUrl}${pictureId}`} className="slideshow-image" />
                     </div>
                 })}
-
-                <a className="prev" onClick={() => plusSlides(-1)}>&#10094;</a>
-                <a className="next" onClick={() => plusSlides(1)}>&#10095;</a>
-                <div className="dots pt-2">
-                    {pictureIds.map((pictureId, index) => <span className={`dot ${index === slideIndex ? 'active' : ''}`} onClick={() => goToSlide(index)}></span>)}
-                </div>
+                {pictureIds.length > 1 && <>
+                    <a className="prev" onClick={() => plusSlides(-1)}>&#10094;</a>
+                    <a className="next" onClick={() => plusSlides(1)}>&#10095;</a>
+                    <div className="dots pt-2">
+                        {pictureIds.map((pictureId, index) => <span key={`picture-${index}`} className={`dot ${index === slideIndex ? 'active' : ''}`} onClick={() => goToSlide(index)}></span>)}
+                    </div>
+                </>}
             </div>
-            <hr />
         </>
     )
 }
